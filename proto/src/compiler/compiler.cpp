40a9604009f461143f262543f854c3a924240622
jbeal <jbeal@4c76e1c1-4b85-4c65-98a1-c45dc8243d55>
2011-05-16 12:49:03 +0000

    attempting windows exponent fix
    git-svn-id: file:///Users/jakebeal/projects/_fin/proto/svn/proto/trunk@417 4c76e1c1-4b85-4c65-98a1-c45dc8243d55

--- proto/src/compiler/compiler.cpp
+++ proto/src/compiler/compiler.cpp
@@ -94,6 +94,11 @@ uint8_t* NeoCompiler::compile(const char *str, int* len) {
  *  COMPILER OBJECT API                                                      *
  *****************************************************************************/
 NeoCompiler::NeoCompiler(Args* args) : Compiler(args) {
+#ifdef __WIN32__
+  // Ensure floating point numbers are printed unix-style
+  _set_output_format(_TWO_DIGIT_EXPONENT);
+#endif
+
   is_dump_all = args->extract_switch("-CDall");
   is_dump_interpreted = args->extract_switch("-CDinterpreted") | is_dump_all;
   is_dump_analyzed = args->extract_switch("-CDanalyzed") | is_dump_all;
