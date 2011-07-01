9ad5a92331154f337c9c90269e806c903ddabc1d
tcampbell <tcampbell@4c76e1c1-4b85-4c65-98a1-c45dc8243d55>
2011-07-01 18:23:44 +0000

    Tweak plugin_manager.{h,cpp} in preparation for hacking them up.
    git-svn-id: file:///Users/jakebeal/projects/_fin/proto/svn/proto/trunk@459 4c76e1c1-4b85-4c65-98a1-c45dc8243d55

--- proto/src/shared/plugin_manager.cpp
+++ proto/src/shared/plugin_manager.cpp
@@ -1,5 +1,5 @@
 /* Mechanisms for handling plugin registry and loading
-Copyright (C) 2005-2008, Jonathan Bachrach, Jacob Beal, and contributors 
+Copyright (C) 2005-2008, Jonathan Bachrach, Jacob Beal, and contributors
 listed in the AUTHORS file in the MIT Proto distribution's top directory.
 
 This file is part of MIT Proto, and is distributed under the terms of
@@ -12,10 +12,12 @@ in the file LICENSE in the MIT Proto distribution's top directory. */
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "config.h"
 
 #include "plugin_manager.h"
+#include "utils.h"
 
 using namespace std;
 
@@ -31,42 +33,50 @@ ProtoPluginManager::ProtoPluginManager() {
   initialized = false;
 }
 
-const PluginInventory* ProtoPluginManager::get_plugin_inventory() { 
+const PluginInventory *
+ProtoPluginManager::get_plugin_inventory()
+{
   ensure_initialized(NULL);
   return &registry;
 }
 
-void ProtoPluginManager::register_lib(string type,string name,string key,
-                                      ProtoPluginLibrary* lib) {
+void
+ProtoPluginManager::register_lib(string type, string name, string key,
+    ProtoPluginLibrary *lib)
+{
   registry[type][name] = key;
   open_libs[key] = lib;
 }
 
-void ProtoPluginManager::ensure_initialized(Args* args) {
-  if(initialized>=2) return; // idempotent
-  
-  if(initialized==0) {
-    if(!read_registry_file())
-      cerr<<"WARNING: Only default plugins will be loaded.\n";
-    lt_dlinit(); // begin using libltdl library tool
-    initialized=1; // mark initialization complete
+void
+ProtoPluginManager::ensure_initialized(Args *args)
+{
+  if (initialized >= 2)
+    return;                     // Idempotent.
+
+  if (initialized == 0) {
+    if (!read_registry_file())
+      cerr << "WARNING: Only default plugins will be loaded.\n";
+    lt_dlinit();                // Begin using libltdl library tool.
+    initialized = 1;            // Mark initialization complete.
   }
 
-  if(args!=NULL) {
+  if (args != NULL) {
     args->save_ptr();
-    // Check command line to see if any other DLLs are being included locally
-    while(args->extract_switch("--DLL",false)) { 
-      string dll_name = args->pop_next();
-      if(!read_dll(dll_name))
+    // Check command line to see if any other DLLs are being included locally.
+    while (args->extract_switch("--DLL", false)) {
+      const string &dll_name = args->pop_next();
+      if (!read_dll(dll_name))
         { cerr << "Unable to include DLL " << dll_name << endl; }
     }
-    initialized=2; // mark command line processing complete
+    initialized = 2;            // Mark command line processing complete.
     args->restore_ptr();
   }
 }
 
-// helper function
-void split(const string &s, const string &token, vector<string> &segments) {
+void
+split(const string &s, const string &token, vector<string> &segments)
+{
   size_t i = 0;
   size_t j = 0;
   while (string::npos != (j = s.find(token, i))) {
@@ -76,16 +86,18 @@ void split(const string &s, const string &token, vector<string> &segments) {
   segments.push_back(s.substr(i, s.length()));
 }
 
-bool ProtoPluginManager::parse_registry(istream &reg,string overridename) {
+bool
+ProtoPluginManager::parse_registry(istream &reg, string overridename)
+{
   bool parseok = true;
   while (!reg.eof()) {
-    string line; getline(reg, line, '\n'); // get next line of registry
+    string line; getline(reg, line, '\n');
     // Ignore comment lines and empty lines.
     if (line.empty() || '#' == line.at(0)) continue;
-    // If the line is precisely equal to the desired structure, slurp it
+    // If the line is precisely equal to the desired structure, slurp it.
     vector<string> segments; split(line, " ", segments);
     if (segments.size() == 4 && segments[2] == "=") {
-      string libname = (overridename=="")?segments[3]:overridename;
+      string libname = (overridename == "") ? segments[3] : overridename;
       registry[segments[0]][segments[1]] = libname;
     } else {
       cerr << "Unable to interpret registry line '" << line << "'" << endl;
@@ -95,82 +107,106 @@ bool ProtoPluginManager::parse_registry(istream &reg,string overridename) {
   return parseok;
 }
 
-bool ProtoPluginManager::read_dll(string libfile) {
-  // open the library
+bool
+ProtoPluginManager::read_dll(string libfile)
+{
+  // Open the library.
   lt_dlhandle handle = lt_dlopenext(libfile.c_str());
-  if(handle==NULL) 
-    { cerr<<"Could not load plugin library "+libfile+"\n"; return false; }
-  
-  // parse the inventory
-  void *fp = lt_dlsym(handle, "get_proto_plugin_inventory");
-  if(fp==NULL)
-    { cerr << libfile << "not a Proto plugin (no inventory)\n"; return false; }
-  string reg((*((get_inventory_func)fp))());
-  istringstream iss(reg);
-  parse_registry(iss,libfile);
-  
-  // register the library
-  fp = lt_dlsym(handle, "get_proto_plugin_library");
-  if(fp==NULL) {
-    cerr<<"Could not get get_proto_plugin_library from "+libfile; 
+  if (handle == NULL) {
+    cerr << "Could not load plugin library " + libfile + "\n";
     return false;
   }
-  ProtoPluginLibrary* lib = (*((get_library_func)fp))(); // run entry-point fn
+
+  // Parse the inventory.
+  void *fp = lt_dlsym(handle, "get_proto_plugin_inventory");
+  if (fp == NULL) {
+    cerr << libfile << "not a Proto plugin (no inventory)\n";
+    return false;
+  }
+
+  string reg((*((get_inventory_func)fp))());
+  istringstream iss(reg);
+  parse_registry(iss, libfile);
+
+  // Register the library.
+  fp = lt_dlsym(handle, "get_proto_plugin_library");
+  if (fp == NULL) {
+    cerr << "Could not get get_proto_plugin_library from " + libfile + "\n";
+    return false;
+  }
+
+  // Run the entry point.
+  ProtoPluginLibrary *lib = (*((get_library_func)fp))();
   open_libs[libfile] = lib;
-  
+
   return true;
 }
 
-bool ProtoPluginManager::read_registry_file() {
-  string registryFile = PLUGIN_DIR+REGISTRY_FILE_NAME;
-  ifstream fin; fin.open(registryFile.c_str());
-  if(!fin.is_open()) 
-    { cerr<<"Unable to open registry file: "<<registryFile<<endl; return false;}
-  if(!parse_registry(fin)) return false;
-  fin.close(); // Done: close the registry and return
+bool
+ProtoPluginManager::read_registry_file(void)
+{
+  string registryFile = PLUGIN_DIR + REGISTRY_FILE_NAME;
+  ifstream fin(registryFile.c_str());
+  if(!fin.good()) {
+    cerr << "Unable to open registry file: " << registryFile << endl;
+    return false;
+  }
+  if(!parse_registry(fin))
+    return false;
   return true;
 }
 
-ProtoPluginLibrary* ProtoPluginManager::get_plugin_lib(string type, string name,
-                                                       Args* args) {
+ProtoPluginLibrary *
+ProtoPluginManager::get_plugin_lib(string type, string name, Args *args)
+{
   ensure_initialized(args);
-  
-  bool known = registry[type].count(name);
-  if(!known)
-    { cerr<<"No registered library contains "+type+" "+name+"\n"; return NULL; }
-  string libfile = registry[type][name];
 
-  ProtoPluginLibrary* lib;
-  if(open_libs.count(libfile)) { lib = open_libs[libfile]; // use library...
-  } else { // ... or load it in if it's not yet loaded
-    string fullname = PLUGIN_DIR+libfile;
+  if (!registry[type].count(name)) {
+    cerr << "No registered library contains " + type + " " + name + "\n";
+    return NULL;
+  }
+
+  string libfile = registry[type][name];
+  ProtoPluginLibrary *lib;
+  if (open_libs.count(libfile)) {
+    // Use the library...
+    lib = open_libs[libfile];
+  } else {
+    // ...or load it in if it's not yet loaded.
+    string fullname = PLUGIN_DIR + libfile;
     lt_dlhandle handle = lt_dlopenext(fullname.c_str());
-    if(handle==NULL) { 
-      cerr<<"Could not load plugin library "+fullname+"\n"; 
+    if (handle == NULL) {
+      cerr << "Could not load plugin library " + fullname + "\n";
       // Enable (and add right extension) if we need better debug info
-      // dlopen(fullname.c_str(),RTLD_LAZY); cerr<<dlerror()<<endl;
-      return NULL; 
-    }
-    void *fp = lt_dlsym(handle, "get_proto_plugin_library");
-    if(fp==NULL) {
-      cerr<<"Could not get get_proto_plugin_library from "+libfile; 
+      // dlopen(fullname.c_str(), RTLD_LAZY); cerr << dlerror() << endl;
       return NULL;
     }
-    lib = (*((get_library_func)fp))(); // run the entry-point function
+    void *fp = lt_dlsym(handle, "get_proto_plugin_library");
+    if (fp == NULL) {
+      cerr << "Could not get get_proto_plugin_library from " + libfile;
+      return NULL;
+    }
+    // Run the entry point.
+    lib = (*((get_library_func)fp))();
     open_libs[libfile] = lib;
   }
   return lib;
 }
 
-void* ProtoPluginManager::get_sim_plugin(string type, string name, Args* args,
-                                         SpatialComputer* cpu,int n){
-  ProtoPluginLibrary* lib = get_plugin_lib(type,name,args);
-  if(lib==NULL) return NULL;
-  return lib->get_sim_plugin(type,name,args,cpu,n);
+void *
+ProtoPluginManager::get_sim_plugin(string type, string name, Args *args,
+    SpatialComputer *cpu,int n)
+{
+  ProtoPluginLibrary *lib = get_plugin_lib(type,name,args);
+  if (lib == NULL) return NULL;
+  return lib->get_sim_plugin(type, name, args, cpu, n);
 }
 
-void* ProtoPluginManager::get_compiler_plugin(string type,string name,Args* args,Compiler* c){
-  ProtoPluginLibrary* lib = get_plugin_lib(type,name,args);
-  if(lib==NULL) return NULL;
-  return lib->get_compiler_plugin(type,name,args,c);
+void *
+ProtoPluginManager::get_compiler_plugin(string type, string name, Args *args,
+    Compiler *c)
+{
+  ProtoPluginLibrary *lib = get_plugin_lib(type,name,args);
+  if (lib == NULL) return NULL;
+  return lib->get_compiler_plugin(type, name, args, c);
 }
--- proto/src/shared/plugin_manager.h
+++ proto/src/shared/plugin_manager.h
@@ -1,69 +1,80 @@
 /* Mechanisms for handling plugin registry and loading
-Copyright (C) 2005-2008, Jonathan Bachrach, Jacob Beal, and contributors 
+Copyright (C) 2005-2008, Jonathan Bachrach, Jacob Beal, and contributors
 listed in the AUTHORS file in the MIT Proto distribution's top directory.
 
 This file is part of MIT Proto, and is distributed under the terms of
 the GNU General Public License, with a linking exception, as described
 in the file LICENSE in the MIT Proto distribution's top directory. */
 
-#ifndef __PLUGIN_MANAGER__
-#define __PLUGIN_MANAGER__
+#ifndef PROTO_SHARED_PLUGIN_MANAGER_H
+#define PROTO_SHARED_PLUGIN_MANAGER_H
 
+#include <istream>
 #include <map>
-#include "utils.h"
+#include <string>
+
 #include "proto_plugin.h"
 
 /// PluginTypeInventory is a map from plugin name -> containing library name
-typedef map<string,string> PluginTypeInventory;
+typedef std::map<std::string, std::string> PluginTypeInventory;
+
 /// PluginInventory is a map from plugin type -> type inventory
-typedef map<string, PluginTypeInventory> PluginInventory;
+typedef std::map<std::string, PluginTypeInventory> PluginInventory;
+
 /// LibraryCollection is a map from open library name -> object
-typedef map<string, ProtoPluginLibrary*> LibraryCollection;
+typedef std::map<std::string, ProtoPluginLibrary *> LibraryCollection;
+
+class Args;
+class Compiler;
+class SpatialComputer;
 
 class ProtoPluginManager {
  public:
   ProtoPluginManager();
-  
-  /** 
-   * Getters return null if they fail, and as a side effect print failure msgs 
-   * MUST CHECK FOR NULL ON RETURN 
+
+  /**
+   * Getters return null if they fail, and as a side effect print failure msgs
+   * MUST CHECK FOR NULL ON RETURN
    * Used to get simulator Layers, Distributions, and TimeModels
    */
-  void* get_sim_plugin(string type, string name, Args* args, 
-                       SpatialComputer* cpu, int n);
+  void *get_sim_plugin(std::string type, std::string name, Args *args,
+      SpatialComputer *cpu, int n);
 
   /// Used to get compiler extensions, or just the opcode part of a sim plugin
-  void* get_compiler_plugin(string type, string name, Args* args, Compiler* c);
-  
+  void *get_compiler_plugin(std::string type, std::string name, Args *args,
+      Compiler *c);
+
   /// Accessor for get_plugin_inventory
-  const PluginInventory* get_plugin_inventory();
+  const PluginInventory *get_plugin_inventory();
 
   /// Where DLLs go
-  static string const PLUGIN_DIR; 
-  
+  static const std::string PLUGIN_DIR;
+
   /// Where the registry goes
-  static string const REGISTRY_FILE_NAME;
+  static const std::string REGISTRY_FILE_NAME;
 
   /// Where .proto extension files go
-  static string const PLATFORM_DIR; 
+  static const std::string PLATFORM_DIR;
 
   /// The default .proto file for a platform
-  static string const PLATFORM_OPFILE; 
+  static const std::string PLATFORM_OPFILE;
 
-  void register_lib(string type,string name,string key,ProtoPluginLibrary* lib);
-  void ensure_initialized(Args* args);
+  void register_lib(std::string type, std::string name, std::string key,
+      ProtoPluginLibrary *lib);
+  void ensure_initialized(Args *args);
 
  private:
   bool initialized;
   PluginInventory registry;
   LibraryCollection open_libs;
-  ProtoPluginLibrary* get_plugin_lib(string type, string name,Args* args);
-  bool read_dll(string libfile);
-  bool read_registry_file();
-  bool parse_registry(istream &reg,string overridename="");
+  ProtoPluginLibrary *get_plugin_lib(std::string type, std::string name,
+      Args *args);
+  bool read_dll(std::string libfile);
+  bool read_registry_file(void);
+  bool parse_registry(std::istream &reg, std::string overridename="");
 };
 
 /// global manager, initializes on first use
-extern ProtoPluginManager plugins; 
+extern ProtoPluginManager plugins;
 
-#endif // __PLUGIN_MANAGER__
+#endif  // PROTO_SHARED_PLUGIN_MANAGER_H
