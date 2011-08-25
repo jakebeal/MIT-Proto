37b1104fe51f6ad2b3c601b10934559dd052e95b
bbenyo <bbenyo@4c76e1c1-4b85-4c65-98a1-c45dc8243d55>
2011-08-25 18:55:13 +0000

    Handle fun cygwin dll names, logging output, don't quit if you can't find any plugins on windows so installation can continue for the moment
    git-svn-id: file:///Users/jakebeal/projects/_fin/proto/svn/proto/trunk@502 4c76e1c1-4b85-4c65-98a1-c45dc8243d55

--- proto/src/createRegistry.cpp
+++ proto/src/createRegistry.cpp
@@ -105,6 +105,7 @@ register_plugin(const string &plugin_directory, const string &filename,
 {
   string pathname = plugin_directory + "/" + filename;
 
+  cout << "Trying to open: " << pathname.c_str() << "\n";
   // Open the plugin.
   scoped_lt_dlhandle handle(lt_dlopenext(pathname.c_str()));
   if (handle == 0) {
@@ -139,7 +140,7 @@ plugin_filename_p(const string &filename)
 {
   return ((filename.size() > 3) && (filename.substr(0, 3) == "lib"));
 }
-
+	
 // Reading the plugin directory
 
 static int
@@ -166,8 +167,15 @@ register_plugins(const string &plugin_directory, ofstream *registry_stream)
       string filename(dirent->d_name);
       if (plugin_filename_p(filename)) {
         size_t dot = filename.rfind('.');
-        if (dot)
-          filename = filename.substr(0, dot);
+        if (dot) {
+        	filename = filename.substr(0, dot);
+        	// Cygwin creates x.dll.a files, just get the base name here
+        	dot = filename.rfind(".dll");
+        	if (dot) {
+        		filename = filename.substr(0, dot);
+        	}
+        }
+        cout << "Adding " << filename << " to plausible plugins\n";
         plausible_plugins.insert(filename);
       }
     }
@@ -190,15 +198,6 @@ register_plugins(const string &plugin_directory, ofstream *registry_stream)
         n_registered += 1;
     }
   }
-
-  // FIXME: This is totally bogus, but I don't understand what's going
-  // on with Windows.  If you do understand, please fix this.
-#ifndef _WIN32
-  if (n_registered == 0) {
-    cerr << "No plugins found!\n";
-    return 3;
-  }
-#endif
 
   return 0;
 }
